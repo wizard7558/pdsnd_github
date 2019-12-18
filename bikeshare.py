b013da811833338c2a24f3fc504fdefcd585f739
Riley Sorenson <rb.sorenson55@gmail.com>
2019-12-18 10:12:34 -0800

    add print statement regarding Washington's lack of user data

--- bikeshare.py
+++ bikeshare.py
@@ -214,6 +214,7 @@ def trip_duration_stats(df):
 
 def user_stats(df):
     """Displays statistics on bikeshare users."""
+	"""Washington has little user data"""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
